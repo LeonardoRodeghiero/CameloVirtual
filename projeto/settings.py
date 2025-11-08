ff076143e3b84c3071158f44790a10849453e040
unknown <rodeghieroleonardo@gmail.com>
2025-11-07 21:46:10 -0300

    +

--- projeto/settings.py
+++ projeto/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
 # Application definition
 
