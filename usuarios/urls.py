6148a763cc9aefe977a5b61bb9b5495cb76641ee
unknown <rodeghieroleonardo@gmail.com>
2026-02-27 11:22:02 -0300

    colocando infos nos cards de camelô e fazendo redefinicao de senha

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -13,4 +13,11 @@ urlpatterns = [
 
     path('atualizar/perfil/<int:pk>/', PerfilUpdate.as_view(), name="atualizar-perfil"),
     path('excluir/perfil/<int:pk>/', UsuarioDelete.as_view(), name="excluir-perfil"),
+
+
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="usuarios/registration/password_reset.html"), name="reset_password"), 
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="usuarios/registration/password_reset_sent.html"), name="password_reset_done"), 
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="usuarios/registration/password_reset_confirm.html"), name="password_reset_confirm"), 
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="usuarios/registration/password_reset_complete.html"), name="password_reset_complete"),
 ]
